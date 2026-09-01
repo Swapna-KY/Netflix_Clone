694943fda7053fc153626c32a749c92937069dfc
Swapna KY <swapnaky883@gmail.com>
2026-09-01 11:01:34 +0530

    Fix database port for Aiven

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(session({
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'netflix_clone',
